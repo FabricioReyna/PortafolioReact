37468ce15a4401cb04a95be44f2b8a7372069c3e
FabricioReyna <fabricioreynacordes@gmail.com>
2023-07-25 22:58:18 -0300

    Todo

--- src/component/Portafolio/index.jsx
+++ src/component/Portafolio/index.jsx
@@ -11,7 +11,7 @@ const MePorta = () => {
             <div className="proyecto">
             <NavLink href="https://illustrious-medovik-87eb50.netlify.app/" target="_blank" >
              
-              <img src="./src/img/EccomerceReact.png" alt="a" />
+              <img src="https://res.cloudinary.com/dhb9rdaoc/image/upload/v1690336622/zpqgitf3o8k94ezef41n.png" alt="a" />
               <div className="overlay">
                   <h4>Eccomerce React</h4>
                   <p>Vista Previa</p>
@@ -20,7 +20,7 @@ const MePorta = () => {
               </NavLink>
             </div>
             <div className="proyecto">
-              <img src="./src/img/LogFormNeo.png" alt="a" />
+              <img src="https://res.cloudinary.com/dhb9rdaoc/image/upload/v1690335908/isuzf59z2hbiejz6nh1g.png" alt="a" />
               <div className="overlay">
                 <NavLink to="https://loginformneo.netlify.app/" target="_blank">
                   <h4>LoginFrom</h4>
@@ -31,7 +31,7 @@ const MePorta = () => {
             <div className="proyecto">
             <NavLink to="https://musicwebboantek.netlify.app/" target="_blank">
              
-              <img src="./src/img/MusicWeb.png" alt="a" />
+              <img src="https://res.cloudinary.com/dhb9rdaoc/image/upload/v1690335910/u6qwfatppu21rfexyyjs.png" alt="a" />
               <div className="overlay">
                   <h4>Music JavaScript</h4>
                   <p>Vista Previa</p>
@@ -41,7 +41,7 @@ const MePorta = () => {
             </div>
             <div className="proyecto">
             <NavLink to="https://c12-accessbank.vercel.app/" target="_blank">
-              <img src="./src/img/AccesBank.png" alt="a" />
+              <img src="https://res.cloudinary.com/dhb9rdaoc/image/upload/v1690335909/AccesLandig.png" alt="a" />
               <div className="overlay">
                 
                   <h4>AccesBank</h4>
